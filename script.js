ed65f75044445c4357944d8f7dab0df74ecb33d2
Kayla Blosser <kayla.blosser@proton.me>
2025-07-02 19:44:34 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+async function soonestDate() {
+    const response = await fetch('https://date.nager.at/api/v3/PublicHolidays/' + new Date().getFullYear() + '/US', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    );
+
+    const data = await response.json();
+
+    for (const holiday of data) {
+        if (new Date(holiday.date) > new Date()) {
+            return holiday;
+        }
+    }
+}
+
+async function updateHolidayInfo() {
+    const holiday = await soonestDate();
+    const daysUntilHoliday = Math.ceil((new Date(holiday.date) - new Date()) / (1000 * 60 * 60 * 24));
+
+    document.getElementById('holiday-label').innerText = holiday.name;
+    document.getElementById('days-label').innerText = daysUntilHoliday;
+}
+
+updateHolidayInfo();
